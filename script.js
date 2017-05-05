a6f77db715c8d9aaa15f32e6a8c3a2c0b2fb5b54
dohertki <dohertki@oregonstate.edu>
2017-05-05 01:53:13 -0700

    Table finished

--- script.js
+++ script.js
@@ -10,14 +10,31 @@ for(var i = 0; i < 3; i++){
 
 
 var newTable = document.createElement("table");
-for(var x = 0; x < 5; x++){
+    newTable.style.border = "thick solid black";
+for(var y = 0; y < 4; y++){
 	var newRow = document.createElement("tr");
     newRow.style.backgroundColor = 'orange';
+    newRow.style.border = "thin solid black";
     newTable.appendChild(newRow);
+    
+    for(var x = 1; x <= 4; x ++){
+        if(y === 0){
+            var newCell = document.createElement("th");
+            //newCell.textContent = "Here I am";
+            var t = document.createTextNode("Header" + x );
+        }
+        else{
+            var newCell = document.createElement("td");
+            //newCell.textContent = "Here I am";
+            var t = document.createTextNode(x + "," + y);
+
+        }
+        
+        newCell.appendChild(t);
+        newRow.appendChild(newCell);
+
+    }
 }
-
-
-
 document.body.innerHTML = '<div id="mytable">boo!</div>'
 document.getElementById("mytable").appendChild(newTable);
 
