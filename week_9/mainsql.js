b476a11767b1468b49df59f205331c503138f1ff
dohertki <kierind@gmail.com>
2017-06-04 13:07:45 -0700

    form next

--- week_9/mainsql.js
+++ week_9/mainsql.js
@@ -10,17 +10,93 @@ app.use(bodyParser.json());
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host           : 'localhost',
+    user           : 'student',
+    password       :  'Zinter01$',
+    database       : 'student'
+});
+
+
+
 app.set('port',3000);
 
-app.get('/',function(req,res){
-  res.type('text/plain');
-  res.send('Welcome to the main page!');
+
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+     pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.send('home ',context);
+    })
+  });
 });
 
-app.get('/other-page',function(req,res){
-  res.type('text/plain');
-  res.send('Welcome to the other page!');
+
+app.get('/',function(req,res,next){
+
+  var content = {};
+
+
+
+	pool.query('SELECT column_name FROM information_schema.columns WHERE table_name=\'workouts\' ', function(err, rows, fields) {
+		if(err){
+			console.log("error");
+			return;
+		}
+    content.cols = rows;
+    console.log(content.cols);
+
+  });
+
+
+
+	pool.query('SELECT * FROM workouts',function(err, rows, fields) {
+		if(err){
+			console.log("error");
+			return;
+		}
+		
+    content.results = rows;
+
+		res.render('new_row',content);
+	});
+});	
+
+app.get('/handle', function(req,res){
+  res.render('new_row');
 });
+
+app.get('/pling',function(req,res){
+
+  
+  res.type('text/plain');
+  res.send('Welcome to the Jungle!');
+});
+/*copied directly from get post assignment needs a fixing*/
+app.post('/forms', function(req,res){
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  var context = {};
+  context.request = "POST";
+  context.dataList = qParams;
+  res.render('display_table', context);
+});
+
+
+
+
 
 app.use(function(req,res){
   res.type('text/plain');
