f66e8ef9c92f5fb826d0167d70a819b366388543
dohertki <dohertki@oregonstate.edu>
2017-06-09 09:19:37 -0700

    testing file

---
+++ week_9/testing/script.js
@@ -0,0 +1,22 @@
+//script.js
+
+
+
+
+document.getElementById('submitWorkout').addEventListener("click", function(event){
+    event.preventDefault()
+
+
+
+    var req = new XMLHttpRequest();
+
+    req.open("POST","localhost:3000",true)    
+
+
+})
+
+
+
+
+/*Create new XMLHttpRequest object*/
+
